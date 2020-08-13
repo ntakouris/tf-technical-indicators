3d2b22568d3856d1262a723c422470aa174cfff8
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-08-13 13:29:47 +0300

    version: 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="tensorflow-technical-indicators",
-    version="0.1.1",
+    version="0.1.2",
     author="Theodoros Ntakouris",
     author_email="zarkopafilis@gmail.com",
     description="Technical Indicators as TensorFlow Graph Functions",
